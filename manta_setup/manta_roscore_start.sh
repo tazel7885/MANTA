bbe2028e5682dfef9d59cb8ea42952ace415c0cb
heroehsyona <heroehsyona@gmail.com>
2021-11-01 03:11:55 +0000

    test done need to add roslaunch

--- manta_setup/manta_roscore_start.sh
+++ manta_setup/manta_roscore_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source /opt/ros/melodic/setup.bash
+source /opt/ros/noetic/setup.bash
 source /home/ubuntu/catkin_ws/devel/setup.bash
 
 export ROS_MASTER_URI=http://localhost:11311
 export ROS_HOSTNAME=localhost
-/opt/ros/melodic/bin/roscore
+/opt/ros/noetic/bin/roscore
